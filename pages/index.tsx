d23ce8bda46b4f0ed96b9b0d3b6ecae60e7dd300
Matthijs Mud <matthijsmud@gmail.com>
2022-04-07 16:59:11 +0200

    Use "Workbench" layout for the homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,16 @@ import { VFC } from "react";
 import {
 
 } from "@mui/material";
+import { Workbench } from "$/layout/workbench";
 
 const Home: VFC = (props) => {
   return <>
     <Head>
       <title></title>
     </Head>
-    
+    <Workbench>
+      
+    </Workbench>
   </>
 }
 
