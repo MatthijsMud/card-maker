0cc1aa607a3f8d34f8d75bb36919888ff0ca8053
MatthijsMud <MatthijsMud@users.noreply.github.com>
2022-04-07 15:01:31 +0000

    Deploying to gh-pages from @ MatthijsMud/card-maker@d23ce8bda46b4f0ed96b9b0d3b6ecae60e7dd300 🚀

---
+++ _next/static/dxrLHWQQ3nN_jnRWKinl8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/42-832d4c9aab8d4b46.js","static/chunks/pages/index-c85a24bf4ba44325.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
